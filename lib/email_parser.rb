451c179503edb8eefe44a3c9112540e373e61ce5
Khalid T <khalid.tissire@gmail.com>
2020-02-09 09:43:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,4 +3,8 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-lab = 0
+email_addresses = "john@doe.com, person@somewhere.org"
+parser = EmailAddressParser.new(email_addresses)
+ 
+parser.parse
+# => ["john@doe.com", "person@somewhere.org"]
